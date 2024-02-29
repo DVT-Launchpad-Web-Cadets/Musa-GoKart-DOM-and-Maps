ef1b2426bfd9bb8aafd21dd9d1ffaaac2d980b7d
Musa-Mabasa <musa14mabasa@gmail.com>
2024-02-29 23:03:39 +0200

    🚧 Added partial rxjs observables for the runs page

--- src/TS/services/api_calls.ts
+++ src/TS/services/api_calls.ts
@@ -1,6 +1,9 @@
 import API_BASE_URL from '../api/api_Base_Url';
 import { KartRun } from '../models/KartRun';
 import { LapDetails } from '../models/lapDetials';
+import { Subject, switchMap, map, catchError, EMPTY } from 'rxjs';
+import { fromFetch } from 'rxjs/fetch';
+import { fromPromise } from 'rxjs/internal/observable/innerFrom';
 
 export function getFileName(
   callBack: (s: string[]) => void,
@@ -48,3 +51,40 @@ export function getLapInfoCall(
       finallyCallBack();
     });
 }
+
+// rxjs
+
+export const newFetchRequest$ = new Subject<null>();
+
+export const allRunsSubject$ = newFetchRequest$.pipe(
+  switchMap(() =>
+    fromFetch(`${API_BASE_URL}/runs`).pipe(
+      switchMap((res) =>
+        fromPromise(res.json()).pipe(
+          switchMap((filename: string) =>
+            fromFetch(`${API_BASE_URL}/runs/${filename}`).pipe(
+              switchMap((res) => fromPromise(res.json())),
+              map((run: KartRun) => run),
+              catchError((error) => {
+                console.error(error);
+                return EMPTY;
+              }),
+            ),
+          ),
+          catchError((error) => {
+            console.error(error);
+            return EMPTY;
+          }),
+        ),
+      ),
+      catchError((error) => {
+        console.error(error);
+        return EMPTY;
+      }),
+    ),
+  ),
+  catchError((error) => {
+    console.error(error);
+    return EMPTY;
+  }),
+);
--- src/TS/services/runs_service.ts
+++ src/TS/services/runs_service.ts
@@ -1,29 +1,13 @@
-import { KartRun } from '../models/KartRun';
-import { getAllRunsCall, getFileName } from './api_calls';
+import {
+  allRunsSubject$,
+  newFetchRequest$,
+} from './api_calls';
 import { addRunsToDom, stopSpinner } from './runsDomManipulation';
 
-getFileName(
-  (res: string[]) => {
-    getAllRuns(res);
-  },
-  (err: Error) => {
-    console.error(err);
-  },
-);
 
-function getAllRuns(filename: string[]) {
-  if (filename?.[0]) {
-    getAllRunsCall(
-      filename[0],
-      (res: KartRun) => {
-        addRunsToDom(res);
-      },
-      (err: Error) => {
-        console.error(err);
-      },
-      () => {
-        stopSpinner();
-      },
-    );
-  }
-}
+allRunsSubject$.subscribe((res) => {
+  addRunsToDom(res);
+  stopSpinner();
+});
+
+newFetchRequest$.next(null);
