00885144d1ff732ed65bb7351942d61fb09142c2
Musa-Mabasa <musa14mabasa@gmail.com>
2024-03-02 18:05:22 +0200

    💄 Added tailwind styles on 2 pages

--- src/TS/services/runsDomManipulation.ts
+++ src/TS/services/runsDomManipulation.ts
@@ -8,18 +8,18 @@ export function addRunsToDom(res: KartRun) {
     const listItem = document.createElement('li');
 
     const date = changeDateFormat(res.date);
+    listItem.setAttribute(
+      'class',
+      'bg-secondaryColor border border-borderColor  p-4 h-56 rounded-md',
+    );
 
     listItem.innerHTML = `
-            <a href="runInfo.html">
-                <div class="run-item">
-                    <div class="main-card-content">
-                        <p class="driver-name"> ${res.driver} </p>
-                        <p class="track-name"> ${res.trackName} • ${res.lapSummaries.length} laps </p>
-                    </div>
-                    <div class="card-tail">
-                        <p class="run-date">${date}</p>
-                        <span>•</span>
-                    </div>
+            <a href="runInfo.html" class="h-full block">
+                <div class="run-item h-full flex flex-col justify-evenly">
+                    <p class="driver-name text-headerColor self-center text-lg font-bold"> ${res.driver} • ${res.sessionName} </p>
+                    <p class="track-name"> <i class="fa fa-map-marker text-accentColor" aria-hidden="true"></i> ${res.trackName} • ${res.lapSummaries.length} laps </p>
+                    <p class="run-date"><i class="fa fa-calendar text-accentColor" aria-hidden="true"></i> ${date}</p>
+                    <p class="run-date"><i class="fa fa-clock-o  text-accentColor" aria-hidden="true"></i> ${res.time}</p>
                 </div>
             </a>`;
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html'],
-  theme: {},
+  content: ['./index.html', './src/**/*.{html,ts}'],
+  theme: {
+    colors:{
+      primaryColor: '#10172a',
+      secondaryColor: '#1e293b',
+      accentColor: '#ed3240',
+      primaryTextColor: 'whitesmoke',
+      borderColor: '#1f2739',
+      headerColor: '#73c2e9'
+    }
+  },
   plugins: [],
 };
